4ea18d748172aadeeb5dd887bc58219fd26329a4
Willian <williamhernandez450@gmail.com>
2024-01-07 17:40:36 -0600

    Basic operations CRUD the employee

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
@@ -32,7 +35,54 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
-
+	
+	@GetMapping("/showFormAdd")
+	public String showFormAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+		
+		theModel.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		
+		// get the employee from the service
+		Employee employee = employeeService.findById(theId);
+		
+		// set employee in the model to propulate the form
+		theModel.addAttribute("employee", employee);
+		
+		// send over to our form
+		
+		return "employees/employee-form";
+		
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		// save the employee
+		employeeService.save(employee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+		
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+		
+		// delete the employee
+		employeeService.deleteById(theId);
+		
+		// redirect to the /employees/list
+		return "redirect:/employees/list";
+		
+	}
 }
--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,10 +1,16 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 
 }
